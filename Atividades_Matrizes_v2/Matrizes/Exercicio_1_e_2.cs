561cf116af242feaacc4ab54dc6133382f03c7cf
Luis-Ricardo-Carvalho <luisricardocarvalholin@gmail.com>
2024-10-31 09:55:21 -0300

    Add files via upload

---
+++ Atividades_Matrizes_v2/Matrizes/Exercicio_1_e_2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Biblioteca_Matriz;
+
+namespace Matrizes
+{
+    class Exercicio_1_e_2
+    {
+        static int maiorValorMatriz(int[,] mat) 
+        {
+            int rows = mat.GetLength(0); //pega linhas
+            int cols = mat.GetLength(1); //pega colunas
+            int maior = mat[0,0];
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j < cols; j++)
+                {
+                    if (mat[i,j]>maior)
+                    {
+                        maior = mat[i,j];
+                    }
+                }//fim for j
+            }//fim for i
+            return maior;
+
+        }
+        static void Main()
+        {
+            int rows, cols;
+            Console.WriteLine("Quantidade de linha e colunas:");
+            rows = int.Parse(Console.ReadLine());
+            cols = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[rows, cols]; // criando matrix
+            Matriz.gera(matrix);
+            Console.WriteLine("Matriz Gerada");
+            Matriz.mostra(matrix);
+            Console.WriteLine($"\nO maior número e {maiorValorMatriz(matrix)}");
+            Console.ReadKey();
+        }
+    }
+}
